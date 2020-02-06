09c8a1f4908eb5650eb230216daa9fe262beecb2
luis <alejandroluis444@gmail.com>
2020-02-05 22:36:38 -0500

    host allw

--- RegistroMascota/views.py
+++ RegistroMascota/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
+from .models import *
+
+
+class MostrarFotos(View):
+    def get(self, request):
+        datos = Mascota.objects.all()
+        return render(request, 'RegistroMascota/fotos.html', {"datos":datos})
+
 # Create your views here.
