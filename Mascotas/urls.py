13cae9e5ac808e10d1222fa6448336ebd87c8266
luis <alejandroluis444@gmail.com>
2020-02-05 22:49:54 -0500

    host allw

--- Mascotas/urls.py
+++ Mascotas/urls.py
@@ -18,7 +18,7 @@ from django.urls import path
 
 from django.conf import settings
 from django.conf.urls.static import static
-from .views import *
+from RegistroMascota.views import *
 
 urlpatterns = [
     path('', admin.site.urls),
