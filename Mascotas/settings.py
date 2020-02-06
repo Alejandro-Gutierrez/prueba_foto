68dc55bb0badbcfcbf5e2ac7a4cd333de9e89127
luis <alejandroluis444@gmail.com>
2020-02-05 21:29:45 -0500

    host allw

--- Mascotas/settings.py
+++ Mascotas/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '!xfmzzsyb#h=71anrgicl7z(j=x=bugw*etv7k62rc=!w3bc)q'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['alex12.pythonanywhere.com']
 
 
 # Application definition
